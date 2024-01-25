4dab87a510f3bf54453834690897419ca4e27151
Volodymyr Savchuk <savchuk.v03@gmail.com>
2024-01-25 22:22:43 +0200

    Apply styling to previously designed layout using TailwindCSS

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -3,19 +3,19 @@ import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from "./MenuItem";
 
 function Menu() {
-   const menu = useLoaderData();
-   return (
-      <ul>
-         {menu.map((item) => (
-            <MenuItem pizza={item} key={item.id} />
-         ))}
-      </ul>
-   );
+  const menu = useLoaderData();
+  return (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((item) => (
+        <MenuItem pizza={item} key={item.id} />
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
-   const menu = await getMenu();
-   return menu;
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
